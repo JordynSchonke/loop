5061fc59335e641bd4ea9d64af4a26b33424cbb4
JordynSchonke <rachel.schonke@gmail.com>
2024-12-20 17:55:24 +0100

    Add files via upload

---
+++ rivision/loop.py
@@ -0,0 +1,7 @@
+for i in range(1,11):
+    print(i)
+name=input("what is your name ?")
+for i in range(2):
+    print("happy birthday to you")
+print("happy birthday to you dear "+name)
+print("happy birthday to you")
---
+++ rivision/main.py
@@ -0,0 +1,15 @@
+print("my name is Jordyn")
+age=12
+print("my age is "+str(age))
+color="purple"
+print("my favoriete color is "+color)
+weight=36
+print("i weight "+str(weight)+"kg")
+
+name=input("what is your name ?")
+print("my name is "+name)
+live=input("where do you live ?")
+print( "I live "+live)
+birthday=int(input("when where you born ?"))
+print(birthday)
+
